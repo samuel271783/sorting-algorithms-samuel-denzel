95b078de625a864965f343d344f852f1d5a7f6ef
Samuel Christian Saputra <samuel.christian.saputra_2026@nyjc.edu.sg>
2026-06-30 07:50:26 +0000

    bubble sort insertion sort done

--- main.py
+++ main.py
@@ -4,18 +4,36 @@ def bubble_sort(arr: list) -> None:
     Args:
         arr: list to be sorted (modified in-place)
     """
-    # Replace the code below with your implementation
-    raise NotImplementedError
+    n = len(arr)
+    for i in range(n-1):
+        check_flag = False
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
+                check_flag = True
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+        if not check_flag:
+            break
+        
 
 
 def insertion_sort(arr: list) -> None:
     """Sort the array using insertion sort (in-place).
-
     Args:
         arr: list to be sorted (modified in-place)
     """
-    # Replace the code below with your implementation
-    raise NotImplementedError
+    length = len(arr)
+    for i in range(1,length):
+        target = arr[i]
+        j = i - 1
+        while j >= 0 and arr[j] > target:
+            arr[j + 1] = arr[j]
+            j = j - 1
+        arr[j + 1] = target
+    
+
+    
 
 
 def merge_sort(arr: list) -> list:
